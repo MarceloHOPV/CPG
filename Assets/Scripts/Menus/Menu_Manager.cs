45f8aec5f665abbe3591031d01e05c24ce773955
MARCELO HENRIQUE DE OLIVEIRA PEREIRA VILELA <marcelo.henrique@gec.inatel.br>
2024-05-18 21:21:51 -0300

    Adicionando a tela inicial final

--- Assets/Scripts/Menus/Menu_Manager.cs
+++ Assets/Scripts/Menus/Menu_Manager.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Menu_Manager : MonoBehaviour
 {
 
     [SerializeField] private GameObject painelMenuInicial;
-    [SerializeField] private GameObject painelSettings;
+    [SerializeField] private GameObject painelSettings; 
+    public AudioSource audioSource;
 
-    
+    public Slider volumeSlider;
+    float sliderValue = 0f;
 
+    void Start()
+    {
+        // Inicializa o slider na posição de 50% (0.5) que é a metade do volume máximo
+        volumeSlider.value = 0.5f;
+        // Adiciona um ouvinte para o evento de mudança de valor do slider
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        sliderValue = volumeSlider.value;
+        audioSource.volume = sliderValue;
+    }
     public void openSetings()
     {
         painelMenuInicial.SetActive(false);
